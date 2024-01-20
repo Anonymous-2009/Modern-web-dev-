961b8c60d3b9ea20bac99078254d29f2e9639b29
Anonymous <krishnabag751@gmail.com>
2024-01-20 17:27:48 +0000

    project complete and learn locomotive js

--- script.js
+++ script.js
@@ -3,6 +3,33 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+function firstPageAnim() {
+  var tl = gsap.timeline();
+
+  tl.from("#nav", {
+    y: "-10",
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo.easeInOut,
+  })
+    .to(".bounding-element", {
+      y: 0,
+      ease: Expo.easeInOut,
+      duration: 2,
+      delay: -1,
+      stagger: 0.5,
+    })
+    .from("#hero-footer", {
+      y: -10,
+      opacity: 0,
+      duration: 1.5,
+      delay: -1,
+      ease: Expo.easeInOut,
+    });
+}
+
+firstPageAnim()
+
 // script for cicele mouse follower 
 /*function circleMouseFollower(){
   window.addEventListener('mousemove', function(details){
