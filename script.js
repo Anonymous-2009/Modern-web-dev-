de83956749a56e90b0f35e0bc10f40d634727b16
anonymous <krishnabag751@gmail.com>
2024-01-20 14:41:52 +0000

    new changes coming soon

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+// script for cicele mouse follower 
+/*function circleMouseFollower(){
+  window.addEventListener('mousemove', function(details){
+   document.querySelector('.circle').style.transform = `translate(${details.clientX}px, ${details.clientY}px)`;
+
+    }
+}*/
+
